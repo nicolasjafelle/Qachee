2ff93a6ea82d5176b3155ddfc7bfef590355092b
Nicolas Jafelle <nicolasj@vsisoft.com>
2014-02-12 23:55:18 -0300

    continue working in QacheeManager

--- QacheeProject/Qachee/src/main/java/com/android/qachee/QacheeManager.java
+++ QacheeProject/Qachee/src/main/java/com/android/qachee/QacheeManager.java
@@ -3,13 +3,16 @@ package com.android.qachee;
 
 import android.support.v4.util.LruCache;
 
+import java.util.List;
+
+
 /**
  * QacheeManager
  * Created by nicolas on 2/11/14.
  */
 public class QacheeManager {
 
-	private LruCache<Long, QacheeObject> qachee;
+	private LruCache<Long, Qacheeable> qachee;
 
 	// Private constructor prevents instantiation from other classes
 	private QacheeManager() {
@@ -17,9 +20,7 @@ public class QacheeManager {
 		// Use 1/8th of the available memory for this memory cache.
 		final int cacheSize = maxMemory / 8;
 
-		qachee = new LruCache<Long, QacheeObject>(cacheSize);
-
-
+		qachee = new LruCache<Long, Qacheeable>(cacheSize);
 	}
 
 	/**
@@ -34,24 +35,35 @@ public class QacheeManager {
 		return SingletonHolder.instance;
 	}
 
-	public LruCache<Long, QacheeObject> getQachee() {
+	public LruCache<Long, Qacheeable> getQachee() {
 		return qachee;
 	}
 
-	public void setQachee(LruCache<Long, QacheeObject> qachee) {
+	public Qacheeable getQacheeable(Long key) {
+		return qachee.get(key);
+	}
+
+	public void setQachee(LruCache<Long, Qacheeable> qachee) {
 		this.qachee = qachee;
 	}
 
-	public void addToQachee(Long key, QacheeObject value) {
-		this.qachee.put(key, value);
+	public void setQachee(List<Qacheeable> qachee) {
+		for(Qacheeable qacheeable : qachee) {
+			this.qachee.put(qacheeable.getKey(), qacheeable);
+		}
 	}
 
-	public void removeFromQachee(Long key) {
-		this.qachee.remove(key);
+	public void addToQachee(Qacheeable qacheeable) {
+		this.qachee.put(qacheeable.getKey(), qacheeable);
+	}
+
+	public void removeFromQachee(Qacheeable qacheeable) {
+		this.qachee.remove(qacheeable.getKey());
 	}
 
 	public void clearQachee() {
 		this.qachee.evictAll();
 	}
+
 
 }
---
+++ QacheeProject/Qachee/src/main/java/com/android/qachee/Qacheeable.java
@@ -0,0 +1,9 @@
+package com.android.qachee;
+
+/**
+ * Created by nicolas on 2/12/14.
+ */
+public interface Qacheeable {
+
+	Long getKey();
+}
